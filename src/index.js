d5a5d65da9f112048a0521e88ccd3485612d3799
IvanVillafane <matheobenjamin99@gmail.com>
2022-01-29 00:52:27 -0300

    vercel2

--- src/index.js
+++ src/index.js
@@ -18,8 +18,7 @@ const port = process.env.PORT || 4000;
 app.listen(port);
 app.get("/",(req, res, )=>{res.send("la pagina Inicio");});
 console.log( `Listen on Port ${port}`);
-module.exports = (req, res) => {
-  res.status(404).json({ message: 'Not Found' });
-};
+
+
 reportWebVitals();
 
